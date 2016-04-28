ff4e2c84c6c68fa53711dc59b587487a1bce1b44
Nathan Macnamara <nathan.macnamara@datasift.com>
2016-04-28 16:04:10 +0100

    Updating tests, switch to phpunit

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Feature Flags
 
 [![Latest Version on Packagist](https://img.shields.io/packagist/v/datasift/feature.svg?style=flat-square)](https://packagist.org/packages/datasift/feature)
 [![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
-[![Build Status](https://travis-ci.org/datasift/php-lib-feature.svg?branch=master)](https://travis-ci.org/datasift/php-lib-feature)
+[![Build Status](https://img.shields.io/travis/datasift/php-lib-feature/master.svg?style=flat-square)](https://travis-ci.org/datasift/php-lib-feature)
 [![Total Downloads](https://img.shields.io/packagist/dt/datasift/feature.svg?style=flat-square)](https://packagist.org/packages/datasift/feature)
 
 Drivers
---
+++ tests/FeatureManagerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace DataSift\Feature;
+
+use PHPUnit_Framework_TestCase;
+use \Mockery as m;
+
+class FeatureManagerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Tear down after tests
+     */
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_is_initializable()
+    {
+        $driver = m::mock('\DataSift\Feature\Driver\Services\DriverInterface');
+        $featureManager = new FeatureManager($driver);
+        $this->assertInstanceOf('DataSift\Feature\FeatureManager', $featureManager);
+    }
+
+    /** @test */
+    public function it_should_call_is_enabled()
+    {
+        $driver = m::mock('\DataSift\Feature\Driver\Services\DriverInterface');
+        $driver->shouldReceive('get')
+            ->once()
+            ->withAnyArgs(array("value", false));
+
+        $featureManager = new FeatureManager($driver);
+        $featureManager->isEnabled("value");
+    }
+
+    /**
+     * DataProvider for falsey values
+     *
+     * @return array
+     */
+    public function falseyProvider()
+    {
+        return array(
+            array('false'),
+            array('')
+        );
+    }
+
+    /**
+     * @dataProvider falseyProvider
+     *
+     * @param string $value
+     *
+     * @test
+     */
+    public function it_should_handle_falsey_values($value)
+    {
+        $driver = m::mock('\DataSift\Feature\Driver\Services\DriverInterface');
+        $driver->shouldReceive('get')
+            ->once()
+            ->withAnyArgs(array("value", false))
+            ->andReturn($value);
+
+        $featureManager = new FeatureManager($driver);
+
+        $this->assertFalse($featureManager->isEnabled('value'));
+    }
+}
