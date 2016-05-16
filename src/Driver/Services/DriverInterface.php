a3e0e7ade68cb82f25bbd7221ca05f441b6567ff
Nathan Macnamara <nathan.macnamara@datasift.com>
2016-05-16 15:07:51 +0100

    Removing comments from interface

--- src/Driver/Services/DriverInterface.php
+++ src/Driver/Services/DriverInterface.php
@@ -13,23 +13,4 @@ interface DriverInterface
      * @return mixed
      */
     public function get($key, $default = false);
-
-//    /**
-//     * Create/Update the value for a given key
-//     *
-//     * @param string $key
-//     * @param string $value
-//     *
-//     * @return void
-//     */
-//    public function put($key, $value);
-//
-//    /**
-//     * Delete the value for a given key
-//     *
-//     * @param string $key
-//     *
-//     * @return mixed
-//     */
-//    public function delete($key);
 }
